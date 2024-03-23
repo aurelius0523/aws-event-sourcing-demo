ac4cfedda7bd7b1722aa3e2a7c6d789c04588dba
aurelius0523 <aurelius0523@gmail.com>
2024-03-23 11:42:17 +0800

    Initial commit

---
+++ test/aws-event-sourcing-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsEventSourcingDemo from '../lib/aws-event-sourcing-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-event-sourcing-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsEventSourcingDemo.AwsEventSourcingDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
